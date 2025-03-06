60b92a0874a70e23650aadbb4df3fc2cec227cd5
JackLikesHotpot <47116970+JackLikesHotpot@users.noreply.github.com>
2025-03-06 02:34:07 +0000

    added vitest and removed references to buffer.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-import Buffer from 'buffer'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  define: {
-    'global.Buffer': Buffer, // Polyfill Buffer in the global scope
-  },
-  optimizeDeps: {
-    include: ['buffer'], // Make sure buffer is included in the dependencies
-  },
+  test: {
+    environment: 'jsdom',
+  }
 })
